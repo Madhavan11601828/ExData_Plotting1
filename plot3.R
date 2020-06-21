448a5c06867dba546175bcf1bb86a57a68bef399
Madhavan11601828 <47300503+Madhavan11601828@users.noreply.github.com>
2020-06-21 12:52:21 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -3,9 +3,7 @@ library("data.table")
 setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
 
 #Reads in data from file then subsets data for specified dates
-powerDT <- data.table::fread(input = "household_power_consumption.txt"
-                             , na.strings="?"
-)
+powerDT <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
 
 # Prevents Scientific Notation
 powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
